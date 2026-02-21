9195b5183cb757e4f8a0dec2e109396cd47f3cd6
omkarkhoche3448 <omkarkhoche3448@gmail.com>
2026-02-21 15:37:22 +0530

    feat: update styles and fonts in index.css, modify title and add font links in index.html

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Link } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import AppRouter from "./AppRouter.jsx";
 import Navbar from "./components/navbar/Navbar";
 
